04df30bf3215adf39cb18d95af09891a1bd8a23e
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-06 08:34:10 -0400

    refactor: add new properties for Shoe

--- demo/lib/features/home/domain/entities/shoe.dart
+++ demo/lib/features/home/domain/entities/shoe.dart
@@ -4,12 +4,31 @@ class Shoe {
   final String brand;
   final String image;
   final int price; 
+  final String description;
+  final double rating;
+  final List<ShoeSize> sizes;
+  final String category;
   
-  Shoe({
+  const Shoe({
     required this.id,
     required this.name,
     required this.brand,
     required this.image,
     required this.price,
+    required this.description,
+    required this.rating,
+    required this.sizes,
+    required this.category,
   });
+}
+
+class ShoeSize {
+  final String size;
+  final int stock;
+
+  ShoeSize({
+    required this.size,
+    required this.stock,
+  });
+  
 }
