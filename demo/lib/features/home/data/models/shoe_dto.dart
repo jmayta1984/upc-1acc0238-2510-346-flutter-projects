c18c4eff8a71c2ea133ffecab9fa422c324d2f80
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-06 08:30:25 -0400

    refactor: add new properties for ShoeDto

--- demo/lib/features/home/data/models/shoe_dto.dart
+++ demo/lib/features/home/data/models/shoe_dto.dart
@@ -6,13 +6,19 @@ class ShoeDto {
   final String brand;
   final String image;
   final int price;
+  final String description;
+  final double rating;
+  final List<ShoeSizeDto> sizes;
 
-  ShoeDto({
+  const ShoeDto({
     required this.id,
     required this.name,
     required this.brand,
     required this.image,
     required this.price,
+    required this.description,
+    required this.rating,
+    required this.sizes,
   });
 
   factory ShoeDto.fromJson(Map<String, dynamic> json) {
@@ -22,10 +28,33 @@ class ShoeDto {
       brand: json['brand'],
       image: json['image'],
       price: json['price'],
+      description: json['description'],
+      rating: json['rating']?.toDouble() ?? 0.0, 
+      sizes: (json['sizes_available'] as List)
+          .map((size) => ShoeSizeDto.fromJson(size as Map<String, dynamic>))
+          .toList(),
     );
   }
 
   Shoe toDomain() {
     return Shoe(id: id, name: name, brand: brand, image: image, price: price);
   }
+}
+
+class ShoeSizeDto {
+  final double size;
+  final int stock;
+
+  const ShoeSizeDto({
+    required this.size,
+    required this.stock,
+  });
+
+  factory ShoeSizeDto.fromJson(Map<String, dynamic> json) {
+    return ShoeSizeDto(
+      size: json['size'],
+      stock: json['quantity']?.toDouble() ?? 0.0
+    );
+  }
+  
 }
