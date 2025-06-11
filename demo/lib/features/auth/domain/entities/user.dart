60bb7e8b047b55f6232b5c554ebf0e2e1f3071b7
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-11 09:06:10 -0400

    feat (Auth): add User

---
+++ demo/lib/features/auth/domain/entities/user.dart
@@ -0,0 +1,15 @@
+class User {
+  final int id;
+  final String email;
+  final String username;
+  final String image;
+  final String name;
+
+  const User({
+    required this.id,
+    required this.email,
+    required this.username,
+    required this.image,
+    required this.name,
+  });
+}
