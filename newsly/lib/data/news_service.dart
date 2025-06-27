1427eda024f1630ad6465ba904a469e31d95dd48
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-27 08:45:03 -0400

    chore: update project

--- newsly/lib/data/news_service.dart
+++ newsly/lib/data/news_service.dart
@@ -8,12 +8,12 @@ import 'package:http/http.dart' as http;
 class NewsService {
   Future<List<News>> searchNews(String query) async {
     final uri = Uri.parse(
-      "https://dev.formandocodigo.com/articles.php?description=$query",
+      "https://newsapi.org/v2/everything?q=$query&apiKey=0ded32e65558448784fccdbc73039b0d&page=1&pageSize=20",
     );
     try {
       final response = await http.get(uri);
       if (response.statusCode == HttpStatus.ok) {
-        final List maps = jsonDecode(response.body);
+        final List maps = jsonDecode(response.body)['articles'];
         return maps.map((item) => NewsDto.fromJson(item).toDomain()).toList();
       } else {
         throw Exception('Failed to load news');
